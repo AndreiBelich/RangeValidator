d00b5320de7f9297664b618420741a0ef69fa04c
AndreiBelich <balichandrey89@gmail.com>
2021-04-08 22:04:03 +0300

    add RangeValidator

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,41 @@
 "use strict";
 
+class RangeValidator{
+
+  constructor(from, to){
+    this._from = from;
+    this._to = to;
+  }
+
+  set from(newValue){
+    this._from = newValue;
+  }
+
+  get from(){
+    return this._from;
+  }
+
+  set to(newValue){
+    this._to = newValue;
+  }
+
+  get to(){
+    return this._to;
+  }
+
+  get range(){
+    return [this.from, this.to];
+  }
+
+  validate(num){
+    if(num < this.from || num > this.to){
+      throw new RangeError("Число не выходит в диапазон!");
+    }
+    return num;
+  }
+}
+
+const validator = new RangeValidator(10, 50);
+console.log(validator.range);
+console.log(validator.from);
+console.log(validator.to);
